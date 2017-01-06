0b0b3ea8723777513f3a08b653b795335e08669a
Nezio <nemanjaz1994@gmail.com>
2017-01-06 19:47:35 +0100

    Main page done, JS started

--- APKscan/js/main.js
+++ APKscan/js/main.js
@@ -1,1 +1,23 @@
+var checkboxTos = document.getElementById('checkboxTos');
+var buttonScan = document.getElementById('buttonScan');
+var buttonUpload = document.getElementById('buttonUpload');
+var fileInput = document.getElementById('fileInput');
 
+checkboxTos.onchange = function()
+{
+	buttonScan.disabled = !this.checked;
+};
+
+buttonUpload.onclick = function()
+{
+	fileInput.click();
+	// make a span for name of file to scan
+	// get that text and set it \/\/\/
+	// http://bit.ly/2jk39gc
+};
+
+buttonScan.onclick = function()
+{
+	alert("Scaning...");
+	// open another page
+};
