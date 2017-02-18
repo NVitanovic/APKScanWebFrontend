3c2830c57747ddee9c1ac44865c9533cdcb21c06
Nezio <nemanjaz1994@gmail.com>
2017-02-18 16:13:42 +0100

    Statistics page in progress...

--- APKscan/js/statistics.js
+++ APKscan/js/statistics.js
@@ -103,6 +103,7 @@ $( document ).ready(function()
         }
     });
     
+    // user OS
     var chartOS = new Chart(document.getElementById("chartOS"),
     {
         type: 'pie',
@@ -136,6 +137,107 @@ $( document ).ready(function()
             }
         }
     });
+
+    // file type
+    var chartExt = new Chart(document.getElementById("chartExt"),
+    {
+        type: 'pie',
+        data:
+        {
+            labels: [".apk", "other"],
+            datasets:
+            [{
+                data: [90, 10],
+                backgroundColor:
+                [
+                    "#0078d7",
+                    "#dd4814"
+                ]
+            }]
+        },
+        options:
+        {
+            legend: {labels:{fontColor: "#254357"}},
+            title:
+            {
+                display: true,
+                text: 'File types',
+                fontFamily: "'Open Sans', sans-serif",
+                fontStyle: "400",
+                fontSize: 20,
+                fontColor: "#254357"
+            }
+        }
+    });
+
+    // User browser
+    var chartBrowser = new Chart(document.getElementById("chartBrowser"),
+    {
+        type: 'bar',
+        data:
+        {
+            labels: ["Chrome", "Firefox", "Opera", "Internet Explorer", "Edge", "Safari", "Android browser", "Other"],
+            datasets:
+            [{
+                label: '# of Votes',
+                data: [26, 18, 15, 3, 7, 10, 20, 3],
+                backgroundColor: "#254357"
+            }]
+        },
+        options:
+        {
+            legend: {labels:{fontColor: "#254357"}},
+            scales:
+            {
+                yAxes:
+                [{
+                    ticks:
+                    {
+                        beginAtZero: true,
+                        fontColor: "#254357"
+                    },
+                    gridLines:
+                    {
+                        display: true
+                    }
+                }],
+                xAxes:
+                [{
+                    ticks:
+                    {
+                        fontColor: "#254357"
+                    },
+                    gridLines:
+                    {
+                        display: true
+                    }
+                }]
+            },
+            legend:
+            {
+                display: false
+            },
+            tooltips:
+            {
+                callbacks:
+                {
+                   label: function(tooltipItem)
+                   {
+                          return tooltipItem.yLabel;
+                   }
+                }
+            },
+            title:
+            {
+                display: true,
+                text: 'User browsers',
+                fontFamily: "'Open Sans', sans-serif",
+                fontStyle: "400",
+                fontSize: 20,
+                fontColor: "#254357"
+            }
+        }
+    });
     
 
 
@@ -148,184 +250,3 @@ $( document ).ready(function()
 
 
 
-// templates /////////////////////////////////////////////////////////////////////////
-/*
-// bar chart
-var myChart1 = new Chart(document.getElementById("myChart1"),
-{
-    type: 'bar',
-    data:
-    {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets:
-        [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor:
-            [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderColor:
-            [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options:
-    {
-        legend: {labels:{fontColor: "#254357"}},
-        scales:
-        {
-            yAxes:
-            [{
-                ticks:
-                {
-                    beginAtZero: false,
-                    fontColor: "#254357"
-                },
-                gridLines:
-                {
-                    display: true
-                }
-            }],
-            xAxes:
-            [{
-                ticks:
-                {
-                    fontColor: "#254357"
-                },
-                gridLines:
-                {
-                    display: true
-                }
-            }]
-        },
-        legend:
-        {
-            display: false
-        },
-        tooltips:
-        {
-            callbacks:
-            {
-               label: function(tooltipItem)
-               {
-                      return tooltipItem.yLabel;
-               }
-            }
-        },
-        title:
-        {
-            display: true,
-            text: 'Number of files analysed per day',
-            fontFamily: "'Open Sans', sans-serif",
-            fontStyle: "400",
-            fontSize: 20,
-            fontColor: "#254357"
-        }
-    }
-});
-
-// pie chart
-var myChart2 = new Chart(document.getElementById("myChart2"),
-{
-    type: 'pie',
-    data:
-    {
-        labels: ["Red", "Blue", "Green"],
-        datasets:
-        [{
-            label: '# of things',
-            data: [60, 30, 10],
-            backgroundColor:
-            [
-                'rgba(255, 50, 50, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(45, 206, 101, 1)'
-            ]
-        }]
-    }
-});
-
-// line chart
-var myChart3 = new Chart(document.getElementById("myChart3"),
-{
-    type: 'line',
-    data:
-    {
-        labels: ["lbl1", "lbl2", "lbl3"],
-        datasets:
-        [{
-            label: 'Lines',
-            fill: false,
-            lineTension: 0,
-            data: [60, 30, 90],
-            backgroundColor: ['rgba(55, 150, 250, 1)'],
-            borderColor: ['rgba(55, 150, 250, 1)']
-        }]
-    },
-    options:
-    {
-        scales:
-        {
-            yAxes:
-            [{
-                ticks:
-                {
-                    beginAtZero: false,
-                    fontColor: "#254357"
-                },
-                gridLines:
-                {
-                    display: true
-                }
-            }],
-            xAxes:
-            [{
-                ticks:
-                {
-                    fontColor: "#254357"
-                },
-                gridLines:
-                {
-                    display: true
-                }
-            }]
-        },
-        legend:
-        {
-            display: false
-        },
-        tooltips:
-        {
-            callbacks:
-            {
-               label: function(tooltipItem)
-               {
-                      return tooltipItem.yLabel;
-               }
-            }
-        },
-        title:
-        {
-            display: true,
-            text: 'Number of files analysed per day',
-            fontFamily: "'Open Sans', sans-serif",
-            fontStyle: "400",
-            fontSize: 20,
-            fontColor: "#254357"
-        }
-    }
-});
-*/
